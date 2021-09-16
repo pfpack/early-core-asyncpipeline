27f225224ee38603119ecaebdfb39eb856a23fee
pmosk <pm@ggroupp.ru>
2021-09-16 12:54:11 +0300

    Improve Equals method

--- src/core-asyncpipeline/AsyncPipeline/AsyncPipeline.T/Equality/Equality.Equals.Wrap.cs
+++ src/core-asyncpipeline/AsyncPipeline/AsyncPipeline.T/Equality/Equality.Equals.Wrap.cs
@@ -1,10 +1,12 @@
 #nullable enable
 
+using System.Diagnostics.CodeAnalysis;
+
 namespace System
 {
     partial struct AsyncPipeline<T>
     {
-        public override bool Equals(object? obj)
+        public override bool Equals([NotNullWhen(true)] object? obj)
             =>
             obj is AsyncPipeline<T> other
             && Equals(other);
--- src/core-asyncpipeline/AsyncPipeline/AsyncResultFlow/Equality/Equality.Equals.Wrap.cs
+++ src/core-asyncpipeline/AsyncPipeline/AsyncResultFlow/Equality/Equality.Equals.Wrap.cs
@@ -1,9 +1,14 @@
 #nullable enable
 
+using System.Diagnostics.CodeAnalysis;
+
 namespace System
 {
     partial struct AsyncResultFlow<TSuccess, TFailure>
     {
-        public override bool Equals(object? obj) => obj is AsyncResultFlow<TSuccess, TFailure> other && Equals(other);
+        public override bool Equals([NotNullWhen(true)] object? obj)
+            =>
+            obj is AsyncResultFlow<TSuccess, TFailure> other
+            && Equals(other);
     }
 }
