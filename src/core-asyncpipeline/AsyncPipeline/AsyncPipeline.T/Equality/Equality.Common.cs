46cccc917cfb11ebd26cfc5a87e26bc6e5829fd7
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-05-28 15:00:56 +0300

    Format code

--- src/core-asyncpipeline/AsyncPipeline/AsyncPipeline.T/Equality/Equality.Common.cs
+++ src/core-asyncpipeline/AsyncPipeline/AsyncPipeline.T/Equality/Equality.Common.cs
@@ -6,11 +6,19 @@ namespace System;
 
 partial struct AsyncPipeline<T>
 {
-    private static Type EqualityContract => typeof(AsyncPipeline<T>);
+    private static Type EqualityContract
+        =>
+        typeof(AsyncPipeline<T>);
 
-    private static EqualityComparer<Type> EqualityContractComparer => EqualityComparer<Type>.Default;
+    private static EqualityComparer<Type> EqualityContractComparer
+        =>
+        EqualityComparer<Type>.Default;
 
-    private static EqualityComparer<ValueTask<T>> ValueTaskComparer => EqualityComparer<ValueTask<T>>.Default;
+    private static EqualityComparer<ValueTask<T>> ValueTaskComparer
+        =>
+        EqualityComparer<ValueTask<T>>.Default;
 
-    private static EqualityComparer<CancellationToken> CancellationTokenComparer => EqualityComparer<CancellationToken>.Default;
+    private static EqualityComparer<CancellationToken> CancellationTokenComparer
+        =>
+        EqualityComparer<CancellationToken>.Default;
 }
